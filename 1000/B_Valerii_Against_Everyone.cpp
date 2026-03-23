dd35bf6b96d6ad55a818f9a7ae14fc05fe7d33fd
Shobhit <104744652+Shobhit150@users.noreply.github.com>
2026-03-24 00:30:13 +0530

    1000 series

---
+++ 1000/B_Valerii_Against_Everyone.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+ll mod_add(ll a, ll b) { return (a % MOD + b % MOD) % MOD; }
+ll mod_sub(ll a, ll b) { return (a % MOD - b % MOD + MOD) % MOD; }
+ll mod_mul(ll a, ll b) { return (a % MOD * b % MOD) % MOD; }
+
+void solve() {
+    
+}
+
+int main() {
+    fastio
+
+    int t = 1;
+    // cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
